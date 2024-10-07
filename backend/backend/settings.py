75d1fd238898785cbc229b0cb4b278778ccccca4
m-a-y-h <colloquially.mayh@tutanota.de>
2024-10-07 20:39:28 +0500

    Bugfix settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 nekon = environ.Env(DEBUG=(bool, False))
-environ.Env.read_env(BASE_DIR / '.nekon')
+environ.Env.read_env(BASE_DIR.parent / '.nekon')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
